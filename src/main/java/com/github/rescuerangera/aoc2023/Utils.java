b818bf7f9697b9d306fb75d6db17edfa4382eddf
edward <hammer_head@mail.ru>
2023-12-08 22:57:59 +0400

    feat: more utils

--- src/main/java/com/github/rescuerangera/aoc2023/Utils.java
+++ src/main/java/com/github/rescuerangera/aoc2023/Utils.java
@@ -5,6 +5,13 @@ import java.io.FileReader;
 import java.util.function.Consumer;
 
 public class Utils {
+
+    /**
+     * Executes the given consumer function on a BufferedReader object initialized with the input file path corresponding to the given day.
+     *
+     * @param consumer The consumer function to be executed on the BufferedReader object.
+     * @param day      The day corresponding to the input file.
+     */
     public static void withInputOfDay(Consumer<BufferedReader> consumer, int day) {
         String filePath = "src/main/resources/dayInputs/day" + day;
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -12,5 +19,28 @@ public class Utils {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    /**
+     * Calculates the greatest common divisor (GCD) of two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The GCD of the two numbers.
+     */
+    public static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    /**
+     * Calculates the least common multiple (LCM) of two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The LCM of the two numbers.
+     */
+    public static long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
     }
 }
